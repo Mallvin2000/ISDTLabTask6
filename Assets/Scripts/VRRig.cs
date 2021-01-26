00843540b93f433836e1642ed15b9afc8fb9ed20
Mallvin2000 <64111704+Mallvin2000@users.noreply.github.com>
2021-01-26 18:19:07 +0800

    smoothness updated

--- Assets/Scripts/VRRig.cs
+++ Assets/Scripts/VRRig.cs
@@ -19,6 +19,7 @@ public class VRMap
 
 public class VRRig : MonoBehaviour
 {
+    public float turnSmoothness;
     public VRMap head;
     public VRMap leftHand;
     public VRMap rightHand;
@@ -35,7 +36,7 @@ public class VRRig : MonoBehaviour
     void LateUpdate()
     {
         transform.position = headConstraint.position + headBodyOffset;
-        transform.forward = Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized;
+        transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized,Time.deltaTime * turnSmoothness);
 
         head.Map();
         leftHand.Map();
